099d75b2e83ba608d13504a1c652bbcd32a06032
Linas Zilinskas <l.zilinskas@godeltech.com>
2025-07-06 19:59:36 +0300

    msg

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,21 @@
-const express = require('express');
-import { PORT } from './config/index';
-import { Request, Response } from 'express';
-import cors from 'cors';
+const express = require("express");
+import { PORT } from "./config/index";
+import { Request, Response } from "express";
+import cors from "cors";
 const app = express();
 
 app.use(cors());
-export const start = () =>
-{
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}: http://localhost:${PORT}`);
-    }
-    );
-    app.get("/",(req:Request,res:Response)=>
-    {
-        res.send("Hello, world!");
-    })
+export const start = () => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}: http://localhost:${PORT}`);
+  });
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Hello, world!");
+  });
 
-    app.get('/ping', (req: Request, res: Response) => {
-        res.json({message:'pong'});   
-    });
+  app.get("/ping", (req: Request, res: Response) => {
+    res.json({ message: "pong" });
+  });
 
-
-    console.log("Hello, world!!!!!!!!");
-}
-
-
+  console.log("Hello, world!!");
+};
